0e18d719e4bdc6ecaf0e13bce35b414fbd963fc1
Magdin Stoica <magdin.stoica@sheridancollege.ca>
2024-10-31 14:23:29 -0400

    Hello world intearction with the user in Python

---
+++ InteractiveHelloWorld.py
@@ -0,0 +1,9 @@
+#ask the user for their name
+userName = input("What is your name?")
+
+#create a formarted string with the greeting, 'Hello' and their name
+greeting = f"Hello {userName}!"
+
+#ouput hello using their name (e.g. "Hello <...>")
+#output the formatted greeting
+print(greeting)
